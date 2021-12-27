64858cefc31992bd0add7c566a57241e6bd060c7
김경현 <kyoung1548@gmail.com>
2021-12-27 16:34:19 +0900

    Fix props msg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your 김경현 App" />
+    <HelloWorld msg="테스트 성공" />
   </div>
 </template>
 
